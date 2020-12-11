b0254dafc2ce1fe7122979e310a0335a8f0983f8
Ross Larson <roslarso@amazon.com>
2020-12-10 22:11:06 -0800

    Advent of Code 2020 Day 1 + start of Util lib

---
+++ aoc/aoc_1/src/main.rs
@@ -0,0 +1,38 @@
+use aoc_util;
+use aoc_util::get_numeric_input;
+
+
+fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
+    let nums : Vec<u32> = get_numeric_input("1");
+
+    for i in 0..(nums.len()-1) {
+        for j in (i+1)..nums.len() {
+            if (nums[i] + nums[j]) == 2020 {
+                println!("Part 1: {}", nums[i] * nums[j]);
+                return
+            }
+        }
+    }
+    println!("Part 1: Answer Not found");
+}
+
+fn part2() {
+    let nums : Vec<u32> = get_numeric_input("1");
+
+    for i in 0..(nums.len()-2) {
+        for j in (i+1)..(nums.len()-1) {
+            for k in (j+1)..nums.len() {
+                if (nums[i] + nums[j] + nums[k]) == 2020 {
+                    println!("Part 2: {}", nums[i] * nums[j] * nums[k]);
+                    return
+                }
+            }
+        }
+    }
+    println!("Part 2: Answer Not found");
+}
